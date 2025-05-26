b30472068f5201c0f839a009ed5d217a228fd00f
Sebastian Guerra <143913999+SebastianTadeoGuerraCeron@users.noreply.github.com>
2025-05-26 15:16:30 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import Script from 'next/script';
 
 export const metadata = {
     title: {
@@ -14,6 +15,19 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                {/* Hotjar Tracking Code */}
+                <Script id="hotjar-init" strategy="afterInteractive">
+                    {`
+                        (function(h,o,t,j,a,r){
+                            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                            h._hjSettings={hjid:6416223,hjsv:6};
+                            a=o.getElementsByTagName('head')[0];
+                            r=o.createElement('script');r.async=1;
+                            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                            a.appendChild(r);
+                        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                    `}
+                </Script>
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
@@ -27,3 +41,4 @@ export default function RootLayout({ children }) {
         </html>
     );
 }
+
